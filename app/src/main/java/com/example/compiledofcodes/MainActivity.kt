8cc3c8f0422f6697377552c835241f396ee8eca5
talle <tallesmellog12@gmail.com>
2022-12-22 23:51:07 -0300

    rebuild teste

--- app/src/main/java/com/example/compiledofcodes/MainActivity.kt
+++ app/src/main/java/com/example/compiledofcodes/MainActivity.kt
@@ -17,7 +17,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         setupListeners()
-
     }
     private fun setupListeners(){
         btnViewPager2Estrutura.setOnClickListener {
